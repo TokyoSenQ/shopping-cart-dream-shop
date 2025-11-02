ef6b6cca0c4bd9eb877a16f382c13ff47c0e2910
Jeka <y.kukhar21@gmail.com>
2025-11-02 17:56:43 +0200

    model Product, Category, Image

---
+++ src/main/java/jeka/tsen/shopping_cart_dream_shop/model/Image.java
@@ -0,0 +1,31 @@
+package jeka.tsen.shopping_cart_dream_shop.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Blob;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fileName;
+    private String fileType;
+
+    @Lob
+    private Blob image;
+    private String dowloadUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+}
