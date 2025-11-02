41d4b8d1fcd545d54c28465f07bfbea3a1011977
Jeka <y.kukhar21@gmail.com>
2025-11-02 17:59:02 +0200

    Exception files

---
+++ src/main/java/jeka/tsen/shopping_cart_dream_shop/exceptions/AlreadyExistsExcpetion.java
@@ -0,0 +1,7 @@
+package jeka.tsen.shopping_cart_dream_shop.exceptions;
+
+public class AlreadyExistsExcpetion extends RuntimeException {
+    public AlreadyExistsExcpetion(String message) {
+        super(message);
+    }
+}
