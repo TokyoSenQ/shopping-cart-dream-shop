ec5b20e62ad3c75086804ab04f968e13854b0fd9
Jeka <y.kukhar21@gmail.com>
2025-11-02 17:52:59 +0200

    primo commit : project Spring Boot shopping cart

---
+++ src/main/java/jeka/tsen/shopping_cart_dream_shop/ShoppingCartDreamShopApplication.java
@@ -0,0 +1,13 @@
+package jeka.tsen.shopping_cart_dream_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartDreamShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCartDreamShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jeka/tsen/shopping_cart_dream_shop/ShoppingCartDreamShopApplicationTests.java
@@ -0,0 +1,13 @@
+package jeka.tsen.shopping_cart_dream_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartDreamShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
