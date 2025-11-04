ac9619b25221585ad699e6f6f1bcf4722c8f11af
Jeka <y.kukhar21@gmail.com>
2025-11-04 17:11:06 +0200

    ApiResponse + application.properties

---
+++ src/main/java/jeka/tsen/shopping_cart_dream_shop/response/ApiResponse.java
@@ -0,0 +1,13 @@
+package jeka.tsen.shopping_cart_dream_shop.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class ApiResponse {
+    private String message;
+    private Object data;
+}
+
+
