f7304cacc6a51b131f2b7fe9291a05adc46e02de
Jeka <y.kukhar21@gmail.com>
2025-11-07 12:41:53 +0200

    Model : Order & OrderItem.

---
+++ src/main/java/jeka/tsen/shopping_cart_dream_shop/enums/OrderStatus.java
@@ -0,0 +1,9 @@
+package jeka.tsen.shopping_cart_dream_shop.enums;
+
+public enum OrderStatus {
+    PENDING,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED,
+    CANCELLED
+}
---
+++ src/main/java/jeka/tsen/shopping_cart_dream_shop/model/Order.java
@@ -0,0 +1,32 @@
+package jeka.tsen.shopping_cart_dream_shop.model;
+
+
+import jakarta.persistence.*;
+import jeka.tsen.shopping_cart_dream_shop.enums.OrderStatus;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
+    private LocalDate orderDate;
+    private BigDecimal totalAmount;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<OrderItem> orderItems = new HashSet<>();
+
+}
